7850ab585f37c938eacc0ce8971466587c8f2a47
Anton Efimenko <reloni@ya.ru>
2016-12-11 17:36:13 +0300

    Fix Docker repo name

--- Publish.sh
+++ Publish.sh
@@ -6,7 +6,7 @@ echo "${TRAVIS_TAG}"
 
 if [ "${TRAVIS_TAG}" != "" ]; then
 docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
-export REPO=reloni/todo-todopostgres
+export REPO=reloni/todo-postgres
 export TAG=empty-${TRAVIS_TAG}
 docker build -f Dockerfile.empty -t $REPO:$TAG --label Postgres=${DBVersion} .
 docker tag $REPO:$TAG $REPO:latest
