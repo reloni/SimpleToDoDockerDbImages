d98af6fbe8c26a449e8c1fc18ef80f03dc1f6b61
Anton Efimenko <reloni@ya.ru>
2016-12-11 16:57:43 +0300

    Change config again

--- Publish.sh
+++ Publish.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+travistag=$1
+user=$2
+pass=$3
+dbversion=$4
+
 set -ev
 
-if [ "$TRAVIS_TAG" != "" ]; then
-docker login -u $DOCKER_USER -p $DOCKER_PASS
+if [ "$travistag" != "" ]; then
+docker login -u $user -p $pass
 export REPO=reloni/todo-todopostgres
-export TAG=empty-$TRAVIS_TAG
-docker build -f Dockerfile.empty -t $REPO:$TAG --label Postgres=$DBVersion .
+export TAG=empty-$travistag
+docker build -f Dockerfile.empty -t $REPO:$TAG --label Postgres=$dbversion .
 docker tag $REPO:$TAG $REPO:latest
 docker push $REPO
 fi
