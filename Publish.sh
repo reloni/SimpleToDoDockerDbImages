46ba114d1b96ae964ea169c2188f1827456fbdfa
Anton Efimenko <aefimenko@moysklad.ru>
2017-01-09 11:55:37 +0300

    Test docker image with db password

--- Publish.sh
+++ Publish.sh
@@ -10,6 +10,9 @@ export REPO=reloni/todo-postgres
 export TAG=empty-${TRAVIS_TAG}
 docker build -f Dockerfile.empty -t $REPO:$TAG --label Postgres=${DBVersion} .
 docker tag $REPO:$TAG $REPO:latest
+docker run -d --name firstrun -e POSTGRES_PASSWORD=pass1 $REPO:$TAG
+docker stop firstrun
+docker commit firstrun
 docker push $REPO
 fi
 
