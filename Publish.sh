55159ec1cf62910f4a587aa6ce3a3c904b6f1d86
Anton Efimenko <aefimenko@moysklad.ru>
2017-01-09 12:26:28 +0300

    Test with env password

--- Publish.sh
+++ Publish.sh
@@ -2,16 +2,12 @@
 
 set -ev
 
-echo "${TRAVIS_TAG}"
-
 if [ "${TRAVIS_TAG}" != "" ]; then
 docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
 export REPO=reloni/todo-postgres
 export TAG=empty-${TRAVIS_TAG}
 docker build -f Dockerfile.empty -t base --label Postgres=${DBVersion} .
-#docker build -f Dockerfile.empty -t $REPO:$TAG --label Postgres=${DBVersion} .
-#docker tag $REPO:$TAG $REPO:latest
-docker run -d --name firstrun -e POSTGRES_PASSWORD=pass1 base
+docker run -d --name firstrun -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} base
 docker commit firstrun $REPO:$TAG
 docker stop firstrun
 docker tag $REPO:$TAG $REPO:latest
