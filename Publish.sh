b7de0db0bbaac997dbfc3bcd087bfae412c7f12d
Anton Efimenko <aefimenko@moysklad.ru>
2017-01-09 12:11:50 +0300

    Fixes

--- Publish.sh
+++ Publish.sh
@@ -8,11 +8,13 @@ if [ "${TRAVIS_TAG}" != "" ]; then
 docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
 export REPO=reloni/todo-postgres
 export TAG=empty-${TRAVIS_TAG}
-docker build -f Dockerfile.empty -t $REPO:$TAG --label Postgres=${DBVersion} .
-docker tag $REPO:$TAG $REPO:latest
-docker run -d --name firstrun -e POSTGRES_PASSWORD=pass1 $REPO:$TAG
+docker build -f Dockerfile.empty -t base --label Postgres=${DBVersion} .
+#docker build -f Dockerfile.empty -t $REPO:$TAG --label Postgres=${DBVersion} .
+#docker tag $REPO:$TAG $REPO:latest
+docker run -d --name firstrun -e POSTGRES_PASSWORD=pass1 base
+docker commit firstrun $REPO:$TAG
 docker stop firstrun
-docker commit firstrun
+docker tag $REPO:$TAG $REPO:latest
 docker push $REPO
 fi
 
