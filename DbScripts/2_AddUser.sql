2ed99f127556890c8f867d3408d68b0052540cad
Anton Efimenko <aefimenko@moysklad.ru>
2017-01-09 10:51:57 +0300

    Table updates
    Change PK.
    Change Table names.
    Add TaskUser CreationDate.
    Add delete cascade to Task.Owner.

--- DbScripts/2_AddUser.sql
+++ DbScripts/2_AddUser.sql
@@ -1,2 +1,7 @@
-insert into "todouser" (FirstName, LastName, email, password)
-values ('John', 'Doe', 'john@domain.com', 'ololo')
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+insert into taskuser (UUID, CreationDate, FirstName, LastName, email, password)
+values (uuid_generate_v4(), current_timestamp, 'John', 'Doe', 'john@domain.com', 'ololo');
+
+insert into taskuser (UUID, CreationDate, FirstName, LastName, email, password)
+values (uuid_generate_v4(), current_timestamp, 'Jane', 'Doe', 'jane@domain.com', 'ololo')
