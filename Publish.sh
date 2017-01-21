2266da662c82b33a5ae93a35a851d2aad2fa3917
Anton Efimenko <reloni@ya.ru>
2017-01-21 21:00:38 +0300

    Setup AWS repo

--- Publish.sh
+++ Publish.sh
@@ -5,8 +5,8 @@ set -ev
 echo "${TRAVIS_TAG}"
 
 if [ "${TRAVIS_TAG}" != "" ]; then
-docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
-export REPO=reloni/todo-postgres
+docker login -u ${DOCKER_AWS_USER} -p ${DOCKER_AWS_PASS} -e none ${DOCKER_AWS_URL}
+export REPO=${DOCKER_AWS_REPONAME}
 export TAG=empty-${TRAVIS_TAG}
 docker build -f Dockerfile.empty -t $REPO:$TAG --label Postgres=${DBVersion} .
 docker tag $REPO:$TAG $REPO:latest
