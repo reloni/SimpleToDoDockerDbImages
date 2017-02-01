691e263b645a33f169df8faf704c25057d939b0b
Anton Efimenko <reloni@ya.ru>
2017-02-01 21:34:25 +0300

    Setup correct aws login

--- Publish.sh
+++ Publish.sh
@@ -2,15 +2,13 @@
 
 set -ev
 
-echo "${TRAVIS_TAG}"
-
 if [ "${TRAVIS_TAG}" != "" ]; then
-docker login -u ${DOCKER_AWS_USER} -p ${DOCKER_AWS_PASS} -e none ${DOCKER_AWS_URL}
-export REPO=${DOCKER_AWS_REPONAME}
-export TAG=empty-${TRAVIS_TAG}
-docker build -f Dockerfile.empty -t $REPO:$TAG --label Postgres=${DBVersion} .
-docker tag $REPO:$TAG $REPO:latest
-docker push $REPO
+  eval $(aws ecr get-login --region eu-central-1)
+  export REPO=reloni/todo-postgres
+  export TAG=empty-${TRAVIS_TAG}
+  docker build -f Dockerfile.empty -t $REPO:$TAG --label Postgres=${DBVersion} .
+  docker tag $REPO:$TAG $REPO:latest
+  docker push $REPO
 fi
 
 exit 0;
