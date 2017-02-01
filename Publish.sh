f7577aec861efb7521c19b8bda859a59bce62e2e
Anton Efimenko <reloni@ya.ru>
2017-02-01 21:40:51 +0300

    Fix repo name

--- Publish.sh
+++ Publish.sh
@@ -4,7 +4,7 @@ set -ev
 
 if [ "${TRAVIS_TAG}" != "" ]; then
   eval $(aws ecr get-login --region eu-central-1)
-  export REPO=reloni/todo-postgres
+  export REPO=${DOCKER_AWS_REPONAME}
   export TAG=empty-${TRAVIS_TAG}
   docker build -f Dockerfile.empty -t $REPO:$TAG --label Postgres=${DBVersion} .
   docker tag $REPO:$TAG $REPO:latest
