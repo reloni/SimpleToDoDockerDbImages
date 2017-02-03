ce9cf49e2677bb14fb23ea5713c34f26cdbc0748
Anton Efimenko <reloni@ya.ru>
2017-02-04 00:24:43 +0300

    Setup AWS secrets loading

--- secrets-entrypoint.sh
+++ secrets-entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-eval "$(echo POSTGRES_PASSWORD=supersecretpass | sed 's/^/export /')"
-
-# Load the S3 secrets file contents into the environment variables
-#eval $(aws s3 cp s3://${SECRETS_BUCKET_NAME}/db_credentials.txt - | sed 's/^/export /')
+if [ "$LOAD_S3_SECRETS" = "YES" ]; then
+  # Load the S3 secrets file contents into the environment variables
+  echo "loading secrets"
+  eval $(aws s3 cp s3://${SECRETS_BUCKET_NAME}/${SECRETS_FILE_NAME} - | sed 's/^/export /')
+fi
 
 /docker-entrypoint.sh "$@"
