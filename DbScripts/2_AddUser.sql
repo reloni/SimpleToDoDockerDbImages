1eac9cfb4da92c8047bb5cb756c4c6caebb947a8
Anton Efimenko <reloni@ya.ru>
2017-03-25 17:40:28 +0300

    Delete createuser script

--- DbScripts/2_AddUser.sql
+++
@@ -1,7 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-insert into taskuser (UUID, FirebaseId, CreationDate, FirstName, LastName, email)
-values (uuid_generate_v4(), 'THRbOVbGimVbhrJ5gUqzgzOmNLf2', current_timestamp, 'John', 'Doe', 'reloni@ya.ru');
-
-insert into taskuser (UUID, FirebaseId, CreationDate, FirstName, LastName, email, password)
-values (uuid_generate_v4(), 'testid', current_timestamp, 'Jane', 'Doe', 'jane@domain.com', 'ololo')
